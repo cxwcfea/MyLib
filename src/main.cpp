2096d7ede918e7a6786a51a0561976b7b20c281c
cxwcfea <cxwcfea@163.com>
2014-02-17 09:50:16 -0500

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 17, 2014
+ *      Author: xcheng
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	auto a = 9;
+	cout << "Start " << a << endl;
+}
+
+
+
