acecc7e0e3af0e6cc1afdced34f87267b2475a5c
cxwcfea <cxwcfea@163.com>
2014-02-17 11:26:42 -0500

    add utility functions

---
+++ src/Precompiled.h
@@ -0,0 +1,56 @@
+/*
+ * Precompiled.h
+ *
+ *  Created on: Feb 17, 2014
+ *      Author: xcheng
+ */
+
+#ifndef CXWCFEA_PRECOMPILED_H_
+#define CXWCFEA_PRECOMPILED_H_
+
+#include <cstdio>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+#define ASSERT assert
+
+static const int BUF_SIZE = 256;
+
+struct Tracer {
+	const char *file_name_;
+	unsigned line_num_;
+
+	Tracer(const char *file_name, unsigned line_num) :
+			file_name_(file_name), line_num_(line_num) {
+
+	}
+
+	auto operator()(const char *format, ...) const -> void {
+		char buf[BUF_SIZE];
+		auto count = snprintf(buf, BUF_SIZE, "%s(%d): ", file_name_, line_num_);
+		ASSERT(-1 != count);
+		va_list args;
+		va_start(args, format);
+		ASSERT(-1 != vsnprintf(buf+count, BUF_SIZE-count, format, args));
+		std::cout << buf;
+		va_end(args);
+	}
+};
+
+#ifdef NDEBUG
+#define VERIFY(expression) (expression)
+#define TRACE
+#else
+#define VERIFY ASSERT
+#define TRACE Tracer(__FILE__, __LINE__)
+#endif
+
+template<class T, class ... Ts>
+std::unique_ptr<T> make_unique(Ts&&... ts) {
+	return std::unique_ptr<T>(new T(std::forward<Ts>(ts)...));
+}
+
+#endif /* CXWCFEA_PRECOMPILED_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
  *      Author: xcheng
  */
 
-#include <iostream>
+#include "Precompiled.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
 	auto a = 9;
-	cout << "Start " << a << endl;
+	TRACE("Start %d\n", a);
+	auto b = make_unique<int>(8);
+	TRACE("%d\n", *b);
 }
 
 
